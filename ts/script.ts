78dbece66e421287de9605fe93d5420aec52c413
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-10-11 16:00:52 +0200

    end

--- ts/script.ts
+++ ts/script.ts
@@ -1,17 +1,52 @@
-import Alpine from "alpinejs";
+// import Alpine from "alpinejs";
 
-Alpine.data("myComponent", function (initial: number = 0) {
-  return {
-    compteur: initial,
-    increment() {
-      this.compteur += 1;
-    },
-    decrement() {
-      this.compteur -= 1;
-      if (this.compteur > 0) {
-        (this.$el as HTMLSpanElement).style.display = "none";
-      }
-    },
+// Alpine.data("myComponent", function (initial: number = 0) {
+//   return {
+//     compteur: initial,
+//     increment() {
+//       this.compteur += 1;
+//     },
+//     decrement() {
+//       this.compteur -= 1;
+//       if (this.compteur > 0) {
+//         (this.$el as HTMLSpanElement).style.display = "none";
+//       }
+//     },
+//   };
+// });
+// Alpine.start();
+
+type HTMLAttributes = {
+  img: {
+    alt: string;
   };
-});
-Alpine.start();
+  input: {
+    type?: "text" | "number";
+    name: string;
+  };
+};
+
+type HTMLNode<TagName extends keyof HTMLAttributes> = {
+  tagName: TagName;
+  class?: string;
+  id?: number;
+  attributes?: HTMLAttributes[TagName];
+};
+
+type HTMLShape = HTMLNode<"input"> | HTMLNode<"img">;
+
+const img: HTMLShape = {
+  tagName: "img",
+  class: ".demo",
+  attributes: {
+    alt: "demo",
+  },
+};
+
+const input: HTMLShape = {
+  tagName: "input",
+  attributes: {
+    name: "demo",
+    type: "text",
+  },
+};
